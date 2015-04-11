7e5aa520acdbf750bf6adb61f0786420f5dde6fc
GabeKagan <gabekagan@live.com>
2015-04-11 00:30:52 -0400

    Added some skeleton code and a test
    Tomorrow's goal - Get a working engine loop.

--- js/song_properties.js
+++ js/song_properties.js
@@ -2,10 +2,3 @@
 */
 var TEMPO = 100; //Hardcoded for now, represents tempo in BPM.
 
-//Implement a basic delta function later for smooth operation regardless of FPS and speed.
-//Init:
-//var lastTime = Date.now();
-//Loop:
-//var now = Date.now();
-//var delta = (now - lastTime) / 1000.0;
-//lastTime = now;
