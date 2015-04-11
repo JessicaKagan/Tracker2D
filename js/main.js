9531fd041ddb37f6d3a5fd6ce7d502a94de1cc2c
GabeKagan <gabekagan@live.com>
2015-04-11 13:31:24 -0400

    Basic renderer and more placeholder bugs
    They're all smiley because we have a renderfunction.

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ var FILE_SIZE = [30,23]; //This is how many tiles are on the screen right now. I
 var TILE_SIZE = 24; //This is more cumbersome early on, but I will eventually need to implement zooming, and this might help
 var LEFT_VERTICAL_BAR = [0,0,80,800];
 var BOTTOM_HORIZONTAL_BAR = [80,552,720,48];
+var FIELD_PIXELS = [80,0,800,552];
 
 var fieldContents = new Array(30);
 var rows = FILE_SIZE[0];
@@ -13,6 +14,8 @@ for(var i = 0; i < rows; ++i) {
     fieldContents[i] = new Array(23);
 }
 
+//Global for now.
+var bug1;
 
 
 
@@ -41,45 +44,19 @@ function init() {
     ctx.fillRect(BOTTOM_HORIZONTAL_BAR[0],BOTTOM_HORIZONTAL_BAR[1],BOTTOM_HORIZONTAL_BAR[2],BOTTOM_HORIZONTAL_BAR[3]);
     ctx.fillStyle = "#BBBBBB";
 
-    //Draw boundaries between tiles, vertical first
-    for(var i = 80; i < fieldBoundaries[2]; i += 24) {
-        ctx.beginPath();
-        ctx.moveTo(i,0);
-        ctx.lineTo(i,552);
-        ctx.stroke();
-    }
-    for(var i = 0; i < fieldBoundaries[3]; i += 24) {
-        ctx.beginPath();
-        ctx.moveTo(0,i);
-        ctx.lineTo(800,i);
-        ctx.stroke();
-    }
-
-    //Ach squares.
+    //In the future, we'll pull this information from a save file, if we can.
     fieldContents[1][1] = new Tile("test", undefined, undefined, undefined);
     fieldContents[6][1] = new Tile("test", undefined, undefined, undefined);
     fieldContents[11][1] = new Tile("test", undefined, undefined, undefined);
     fieldContents[16][1] = new Tile("test", undefined, undefined, undefined);
     fieldContents[21][1] = new Tile("test", undefined, undefined, undefined);
     fieldContents[26][1] = new Tile("test", undefined, undefined, undefined);
-    //Painting squares! From an MVC stance this is the "view", I guess.
-    //Our update needs to change the underlying model without going through the view.
-    for(var i = 0; i < FILE_SIZE[0]; ++i){
-        
-        for(var j = 0; j < FILE_SIZE[1]; ++j){
-            
-            if(typeof fieldContents[i][j] === 'object'){
-                paintTile(i,j, "#0000FF");
-            }   
-        }
-    }
       
     document.addEventListener("click", interact);
 
     //Draws a test bug, spawning at tile [0,1] without any behavior.
-    var bug1 = new Bug(fieldBoundaries[0] + (TILE_SIZE*0),fieldBoundaries[1] + (TILE_SIZE*1),null);
+    bug1 = new Bug(fieldBoundaries[0] + (TILE_SIZE*0),fieldBoundaries[1] + (TILE_SIZE*1),null);
     console.log(bug1);
-    bug1.drawBug();
     //Experimentally moving the bug. Needs an implementation that wipes the screen as needed.
     /*
     setInterval(function(){
@@ -92,9 +69,10 @@ function init() {
             if(fieldContents[bugTile[0]][bugTile[1]] != undefined){
                 testSound.play();
             }
-        }
+        } else bug1.x = 80;
     }, 200)
     */
+    window.requestAnimationFrame(render);
 
 }
 
@@ -147,9 +125,39 @@ function main(){
 }
 
 function render(){
+    ctx.clearRect(FIELD_PIXELS[0],FIELD_PIXELS[1],FIELD_PIXELS[2],FIELD_PIXELS[3]); //Use this to refresh everything.
     //Render things in this order:
-    //1. Background
+    //1. Background (Probably doesn't need to be redrawn very often)
     //2. Painted tiles
+
+    //Draw boundaries between tiles.
+    for(var i = 80; i < fieldBoundaries[2]; i += TILE_SIZE) {
+        ctx.beginPath();
+        ctx.moveTo(i,0);
+        ctx.lineTo(i,552);
+        ctx.stroke();
+    }
+    for(var i = 0; i < fieldBoundaries[3]; i += TILE_SIZE) {
+        ctx.beginPath();
+        ctx.moveTo(0,i);
+        ctx.lineTo(800,i);
+        ctx.stroke();
+    }
+    //Painting squares! From an MVC stance this is the "view", I guess.
+    //paintTile eventually needs to choose colors first based on tile properties, and then a subset of it based on user's viewmode.
+    for(var i = 0; i < FILE_SIZE[0]; ++i){
+        for(var j = 0; j < FILE_SIZE[1]; ++j){
+            if(typeof fieldContents[i][j] === 'object'){
+                paintTile(i,j, "#0000FF");
+            }   
+        }
+    }
+
     //3. Bugs
+    bug1.drawBug();
     //4. UI (Seems trivial, but I plan to have translucent popups in the near future.)
+    //2 & 3 are going to take up most of our time.
+
+    //Then call a new frame of animation.
+    window.requestAnimationFrame(render);
 }
