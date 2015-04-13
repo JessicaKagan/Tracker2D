c66a2cfbce4058b71a29e14c306bddff9aecc8ee
GabeKagan <gabekagan@live.com>
2015-04-13 19:45:30 -0400

    More reversion
    Bleah.

--- Tracker2D/js/song_properties.js
+++
@@ -1,5 +0,0 @@
-/* Under construction, derp
-*/
-var TEMPO = 120; //Hardcoded for now, represents tempo in BPM. Use mathematical wizardry to make sure this is accurate.
-var PLAYFIELD_SIZES = [64, 128, 256, 512]; //The user will eventually be able to choose how large their field is in tiles. Produces squares.
-
