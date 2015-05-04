af0c951ddaec4d77c1e88a38bcaf48a6f85d1f74
GabeKagan <gabekagan@live.com>
2015-05-04 12:43:04 -0400

    Added some sounds
    Just for the sake of having something marginally productive.

--- js/soundset.js
+++ js/soundset.js
@@ -9,9 +9,13 @@
 var soundSet = new Array(129); //128 sounds for the general MIDI instruments; index 0 is silence.
 soundSet[0] = ["00 - Silence", './sounds/00.mp3'];
 soundSet[1] = ["01 - Grand Piano", './sounds/01.mp3']; //Name and reference sound for a simple one-sound instrument.
-soundSet[5] = ["05 - DX7", './sounds/05.mp3']; 
+soundSet[5] = ["05 - Cheesy DX7", './sounds/05.mp3']; 
 soundSet[6] = ["06 - Harpsichord", './sounds/06.mp3'];
 soundSet[24] = ["24 - Nylon String Guitar", './sounds/24.mp3']; 
 soundSet[30] = ["30 - Overdriven Guitar", './sounds/30.mp3'];
 soundSet[31] = ["31 - Distorted Guitar", './sounds/31.mp3'];
+soundSet[48] = ["48 - String Section", './sounds/48.mp3'];
+soundSet[80] = ["80 - Square Waveform", './sounds/80.mp3'];
+soundSet[81] = ["81 - Sawtooth Waveform", './sounds/81.mp3'];
+
 //soundSet[i][1] can either be a string, or an array. If it's an array, we'll need to override the pitchtable logic.
