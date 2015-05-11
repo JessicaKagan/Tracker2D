ae988fe8f4a6835eaf2f76386697742eeb227bb6
GabeKagan <gabekagan@live.com>
2015-05-11 01:24:17 -0400

    Added basic percussion
    However, future sound additions will have to wait for an upgrade to the
    instrument menu.

--- js/soundset.js
+++ js/soundset.js
@@ -6,7 +6,7 @@
 
 //Some of these sounds need tweaking. 1 and 5 come to mind.
 //Are there any other parameters that might come in handy?
-var soundSet = new Array(129); //128 sounds for the general MIDI instruments; index 0 is silence.
+var soundSet = new Array(175); //128 sounds for the general MIDI instruments, 46 more for the base percussion map; index 0 is silence.
 soundSet[0] = ["00 - Silence", './sounds/00.mp3'];
 soundSet[1] = ["01 - Grand Piano", './sounds/01.mp3']; //Name and reference sound for a simple one-sound instrument.
 soundSet[5] = ["05 - Cheesy DX7", './sounds/05.mp3']; 
@@ -17,5 +17,14 @@ soundSet[31] = ["31 - Distorted Guitar", './sounds/31.mp3'];
 soundSet[48] = ["48 - String Section", './sounds/48.mp3'];
 soundSet[80] = ["80 - Square Waveform", './sounds/80.mp3'];
 soundSet[81] = ["81 - Sawtooth Waveform", './sounds/81.mp3'];
+//Beginning of the percussion map
+soundSet[128] = ["P01 - Acoustic Bass Drum", './sounds/128.mp3'];
+soundSet[131] = ["P04 - Acoustic Snare", './sounds/131.mp3'];
+soundSet[132] = ["P05 - Hand Clap", './sounds/132.mp3'];
+soundSet[133] = ["P06 - Electric Snare", './sounds/133.mp3'];
+soundSet[135] = ["P08 - Closed Hi Hat", './sounds/135.mp3'];
+soundSet[137] = ["P10 - Pedal Hi Hat", './sounds/137.mp3'];
+soundSet[139] = ["P12 - Open Hi Hat", './sounds/139.mp3'];
+soundSet[142] = ["P15 - Crash Cymbal 1", './sounds/142.mp3'];
 
 //soundSet[i][1] can either be a string, or an array. If it's an array, we'll need to override the pitchtable logic.
